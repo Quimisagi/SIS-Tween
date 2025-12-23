5a1924efc1b55c90d36820294682f9690f6e820f
Quimisagi <quimisagi@gmail.com>
2025-12-23 20:22:07 +0900

    added cross frame attention to the interpolator + refinement of channels sizes

--- models/interp.py
+++ models/interp.py
@@ -43,39 +43,85 @@ class UpSampleBlock(nn.Module):
         x = self.conv(x)
         return x
 
-class Interpolator(nn.Module):
-    def __init__(self, in_channels=6, out_channels=3):
+class CrossFrameAttention(nn.Module):
+    def __init__(self, channels, reduction=8):
         super().__init__()
-        self.enc1 = DownSampleBlock(in_channels, 64)
-        self.enc2 = DownSampleBlock(64, 128)
-        self.enc3 = DownSampleBlock(128, 256)
-        self.enc4 = DownSampleBlock(256, 512)
+        self.channels = channels
+        self.reduction = reduction
+        
+        self.query_conv = nn.Conv2d(channels, channels // reduction, 1)
+        self.key_conv = nn.Conv2d(channels, channels // reduction, 1)
+        self.value_conv = nn.Conv2d(channels, channels, 1)
+        
+        self.out_conv = nn.Conv2d(channels, channels, 1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+        
+    def forward(self, frame1_features, frame2_features):
+        batch_size, channels, height, width = frame1_features.size()
+        
+        query = self.query_conv(frame1_features).view(batch_size, -1, height * width).permute(0, 2, 1)
+        key = self.key_conv(frame2_features).view(batch_size, -1, height * width)
+        value = self.value_conv(frame2_features).view(batch_size, -1, height * width)
+        
+        attention = torch.bmm(query, key)
+        attention = F.softmax(attention, dim=2)
+        
+        attended = torch.bmm(value, attention.permute(0, 2, 1))  
+        attended = attended.view(batch_size, channels, height, width)
+        
+        output = self.gamma * self.out_conv(attended) + frame1_features
+        
+        return output
 
-        self.bottleneck = ConvBlock(512, 1024)
 
-        self.dec4 = UpSampleBlock(1024, 512)
-        self.dec3 = UpSampleBlock(512, 256)
-        self.dec2 = UpSampleBlock(256, 128)
-        self.dec1 = UpSampleBlock(128, 64)
+class Interpolator(nn.Module):
+    """
+    Contains 3 downsampling blocks, a cross-frame attention mechanism, a bottleneck, and 3 upsampling blocks.
+    For downsampling, each frame is processed separately, but for upsampling, skip connections from both frames 
+    are concatenated.
+    """
+    def __init__(self, frame_c, base_c):
+        super().__init__()
+        self.down1 = DownSampleBlock(frame_c, base_c)
+        self.down2 = DownSampleBlock(base_c, base_c * 2)
+        self.down3 = DownSampleBlock(base_c * 2, base_c * 4)
 
-        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+        self.attention = CrossFrameAttention(base_c * 4)
+
+        self.bottleneck = ConvBlock(base_c * 4 * 2, base_c * 8) 
+
+        self.up3 = UpSampleBlock(base_c * 16, base_c * 4)
+        self.up2 = UpSampleBlock(base_c * 8, base_c * 2)
+        self.up1 = UpSampleBlock(base_c * 4, base_c)
+        self.final_conv = nn.Conv2d(base_c, frame_c, kernel_size=1)
         self.tanh = nn.Tanh()
 
+    def forward(self, frame1, frame2):
+        f1_d1, f1_p1 = self.down1(frame1)
+        f1_d2, f1_p2 = self.down2(f1_p1)
+        f1_d3, f1_p3 = self.down3(f1_p2)
 
-    def forward(self, x):
-        d1, p1 = self.enc1(x)
-        d2, p2 = self.enc2(p1)
-        d3, p3 = self.enc3(p2)
-        d4, p4 = self.enc4(p3)
+        f2_d1, f2_p1 = self.down1(frame2)
+        f2_d2, f2_p2 = self.down2(f2_p1)
+        f2_d3, f2_p3 = self.down3(f2_p2)
 
-        b = self.bottleneck(p4)
+        f1_att = self.attention(f1_p3, f2_p3)
+        f2_att = self.attention(f2_p3, f1_p3)
 
-        u4 = self.dec4(b, d4)
-        u3 = self.dec3(u4, d3)
-        u2 = self.dec2(u3, d2)
-        u1 = self.dec1(u2, d1)
+        bottleneck_input = torch.cat([f1_att, f2_att], dim=1)
+        bottleneck = self.bottleneck(bottleneck_input)
 
-        out = self.final_conv(u1)
+        skip3 = torch.cat([f1_d3, f2_d3], dim=1) 
+        up3 = self.up3(bottleneck, skip3)
+
+        skip2 = torch.cat([f1_d2, f2_d2], dim=1)
+        up2 = self.up2(up3, skip2)
+
+        skip1 = torch.cat([f1_d1, f2_d1], dim=1)
+        up1 = self.up1(up2, skip1)
+
+        out = self.final_conv(up1)
         out = self.tanh(out)
 
         return out
+
